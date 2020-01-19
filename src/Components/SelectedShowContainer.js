2280f1e954ca78a7eedfe336333fc606d7b6f3be
Mohammed Ali <dody200039@gmail.com>
2020-01-19 05:16:20 +0000

    Done.

--- src/Components/SelectedShowContainer.js
+++ src/Components/SelectedShowContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Episode from './Components/Episode';
+import Episode from './Episode';
 
 class SelectedShowContainer extends Component {
 
@@ -20,7 +20,7 @@ class SelectedShowContainer extends Component {
   mapEpisodes = () => {
     return this.props.episodes.map((e)=>{
       if (e.season == this.state.selectedSeason){
-        return (<Episode eachEpisode={e} key={e.id}/>)
+        return (<Episode episode={e} key={e.id}/>)
       }
     })
   }
@@ -29,10 +29,10 @@ class SelectedShowContainer extends Component {
     this.setState({ selectedSeason: e.target.value })
   }
 
-
   render() {
     const { selectedShow } = this.props
-
+    // DO NOT USE dangerouslySetInnerHTML in your own personal projects
+    // as this can expose you, this app is 1 exception where we can use this method
     return (
       <div style={{position: "static"}}>
         <h2>{selectedShow.name}</h2>
@@ -51,12 +51,12 @@ class SelectedShowContainer extends Component {
 
 }
 
-export SelectedShowContainer;
+export default SelectedShowContainer;
 
 
 Array.prototype.unique = function() {
-  var arr = [];
-  for(var i = 0; i < this.length; i++) {
+  let arr = [];
+  for(let i = 0; i < this.length; i++) {
     if(!arr.includes(this[i])) {
         arr.push(this[i]);
     }
