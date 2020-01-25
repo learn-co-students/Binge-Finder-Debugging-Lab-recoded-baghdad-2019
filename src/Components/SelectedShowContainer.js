34edd3caf36100ad85dfdacd3a7e40ce1d2bb727
IbrahimMahdi97 <ibrahimmahdi115@gmail.com>
2020-01-25 20:06:04 +0300

    finished

--- src/Components/SelectedShowContainer.js
+++ src/Components/SelectedShowContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Episode from './Components/Episode';
+import Episode from './Episode';
 
 class SelectedShowContainer extends Component {
 
@@ -20,7 +20,7 @@ class SelectedShowContainer extends Component {
   mapEpisodes = () => {
     return this.props.episodes.map((e)=>{
       if (e.season == this.state.selectedSeason){
-        return (<Episode eachEpisode={e} key={e.id}/>)
+        return (<Episode episode={e} key={e.id}/>)
       }
     })
   }
@@ -28,7 +28,6 @@ class SelectedShowContainer extends Component {
   handleSelectionChange = (e) => {
     this.setState({ selectedSeason: e.target.value })
   }
-
 
   render() {
     const { selectedShow } = this.props
@@ -51,12 +50,12 @@ class SelectedShowContainer extends Component {
 
 }
 
-export SelectedShowContainer;
+export default SelectedShowContainer;
 
 
 Array.prototype.unique = function() {
-  var arr = [];
-  for(var i = 0; i < this.length; i++) {
+  let arr = [];
+  for(let i = 0; i < this.length; i++) {
     if(!arr.includes(this[i])) {
         arr.push(this[i]);
     }
